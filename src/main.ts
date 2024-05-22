95c60f0b37a65b31243a701658deaa9ea497f95c
Willian Almeida <willianac01@gmail.com>
2024-05-22 16:54:58 -0300

    feat: user can select receive unit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Client, LocalAuth } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
 import { login } from "./apis/login";
+import { LoginResponse } from './types/LoginResponse';
 
 const wwebVersion = '2.2412.54';
 const client = new Client({
@@ -28,8 +29,10 @@ type UserState = {
 	data: {
 		email?: string,
 		password?: string,
+		logInfo?: LoginResponse
 		transaction?: {
 			amount?: string
+			receiveUnit?: string
 		}
 	}
 }
@@ -38,7 +41,6 @@ const userStates = new Map<string, UserState>();
 
 client.on("message", async message => {
 	const userId = message.from;
-	console.log(userId);
 	let userState: UserState | undefined = userStates.get(userId);
 
 	if(!userState) {
@@ -66,18 +68,46 @@ client.on("message", async message => {
 			case 2:
 				userState.data.password = message.body;
 				client.sendMessage(message.from, "Password Received, wait a moment...")
-				const res = await login(userState.data.email!, userState.data.password)
+				const result = await login(userState.data.email!, userState.data.password)
+				userState.data.logInfo = result
 				
-				if(res.responsestring.StatusCode._text === "-1") {
+				if(result.responsestring.StatusCode._text === "-1") {
 					client.sendMessage(message.from, "Wrong credentials")
 					userState.step = 0
 					break;
 				}
-				client.sendMessage(message.from, `Hi ${res.responsestring.MoneySender.SenderFirstName._text} ${res.responsestring.MoneySender.SenderLastName._text}`)
+				client.sendMessage(message.from, `Hi ${result.responsestring.MoneySender.SenderFirstName._text} ${result.responsestring.MoneySender.SenderLastName._text}`)
 				userState.step = 3
-				break;
 			case 3: 
-				client.sendMessage(message.from, "You are in the step four, congrats!")
+				const optionsResponse = `1. 🇧🇴 Bolivia (BOB)
+				2. 🇧🇴 Bolivia Dolar (BOD)
+				3. 🇬🇭 Ghana (GHS)
+				4. 🇰🇪 Kenya (KES)
+				5. 🇳🇬 Nigeria (NGN)
+				6. 🇵🇪 Peru Dolar (PED)
+				7. 🇵🇪 Peru (PEN)
+				8. 🇵🇭 Philippines Dolar (PHD)
+				9. 🇵🇭 Philippines (PHP)
+				10. 🇺🇬 Uganda (UGX)
+				11. XOF (West African CFA franc)
+				12. 🇿🇲 Zambia (ZAR)`
+
+				setTimeout(() => {
+					client.sendMessage(message.from, "Where do you want to send money? Please, enter the option number.")
+					client.sendMessage(message.from, optionsResponse);
+				}, 1500)
+				userState.step = 4
+				break;
+			case 4: 
+				const option = parseInt(message.body);
+				const units = userState.data.logInfo?.responsestring.LinkInfo.ListLandUnit._text.split(",");
+
+				if(option > 0 && option <= units!.length) {
+					const response = "You selected option: " + units![option - 1];
+					client.sendMessage(message.from, response);
+					break;
+				}
+				client.sendMessage(message.from, "Invalid option. Please enter a valid option number.")
 				break;
 			default:
 				break;
